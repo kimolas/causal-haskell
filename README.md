4bca1b0ab426509bcbd85e28cdcd406d1d259aed
kimolas <nicolask@andrew.cmu.edu>
2015-04-19 22:21:29 -0400

    Included a function to initialize empty graphs, as well as a function to make graphs from edge lists.

--- README.md
+++ README.md
@@ -2,12 +2,8 @@
 
 ## Introduction
 I'm a big fan of Haskell. I also spend some of my time thinking about graphical
-models (and maybe network models). This is my attempt at combining the two. It's
-only my first attempt, so expect the code to be quite slow. 
-
-In the future, I'd like to use a more sane choice of data structure for storing,
-accessing, and searching edges in graphs. I'll probably make a separate branch
-for that at some point, once I have too much work invested in the current model. 
+models. This is my attempt at combining the two. It's only my first attempt, so
+expect the code to be quite slow. 
 
 ## Why Haskell? 
 A monad is just a monoid in the category of endofunctors. This makes Haskell
--- graph.hs
+++ graph.hs
@@ -1,45 +1,62 @@
--- (Kinda) Graphical Models in Haskell. 
--- (Also works for networks, sort of)
+-- Graphical and Network Models in Haskell. 
 -- Nicolas Kim
 -- Department of Statistics, Carnegie Mellon University 
--- 26 March 2015
 
-import qualified Data.Map as Map
-import qualified Data.List as List
+import qualified Data.Map as M
+import qualified Data.List as L
 
--- let gr1 = Graph [1..3] [(1,2), (2,1), (1,3)]
--- let gr2 = Graph [1..2] [(1,2), (2,1), (1,3)]
+gr1 = Graph [1..3] [(1,2), (2,1), (1,3)]
+gr2 = Graph [1..2] [(1,2), (2,1), (1,3)]
+
+-- module MyGraph ( Graph (..)
+--                , Model (..)
+--                , toGraph
+--                ) where
 
 data Graph = Graph { nodes :: [Int]
-                      , edges :: [(Int, Int)]
-                      } deriving (Show, Eq)
+                   , edges :: [(Int, Int)]
+                   } deriving (Show, Eq)
 
-data Model = ERG { statistics :: [Float] } deriving (Show, Eq)
+data Model = ERG { statistics :: [Double] } deriving (Show, Eq)
+
+-- Makes an empty graph
+emptyGraph :: Graph
+emptyGraph = Graph [] []
+
+-- Makes a valid graph object from a list of edges. 
+toGraph :: [(Int, Int)] -> Graph
+toGraph es = Graph (extractNodes es) es
 
 -- Checks if the edges contain defined nodes. 
 validGraph :: Graph -> Bool
-validGraph (Graph ns es) = subset (List.union (map fst es) (map snd es)) ns
-    where
-      subset xs ys = all (`elem` ys) xs
+validGraph (Graph ns es) = subset (L.union (map fst es) (map snd es)) ns
+  where
+    subset xs ys = all (`elem` ys) xs
 
 -- Makes a graph valid (see the `validGraph` function). 
 makeValid :: Graph -> Graph
 makeValid gr@(Graph ns es)
   | validGraph gr = gr
-  | otherwise = (Graph (List.union ens ns) es)
+  | otherwise = (Graph (L.union ens ns) es)
     where
-      ens = List.nub $ List.union (map fst es) (map snd es)
+      ens = L.nub $ L.union (map fst es) (map snd es)
+
+-- From a list of edges, give the sorted and pruned list of nodes
+extractNodes :: [(Int, Int)] -> [Int]
+extractNodes es = L.nub $ L.sort allnodes
+  where
+    allnodes = (map fst es) ++ (map snd es)
 
 -- Add a set of nodes to a graph using their labels. 
 addNodes :: Graph -> [Int] -> Graph
-addNodes (Graph ns es) nns = (Graph (List.union ns nns) es)
+addNodes (Graph ns es) nns = (Graph (L.union ns nns) es)
 
 -- Add a set of edges to a graph. 
 addEdges :: Graph -> [(Int, Int)] -> Graph
-addEdges (Graph ns es) ees = (Graph ns (List.union es ees))
+addEdges (Graph ns es) ees = (Graph ns (L.union es ees))
 
--- Model fitting. 
-fitERG :: Graph -> Model
-fitERG gr@(Graph ns es) = ERG [ density ]
-  where
-    density = (/) (fromIntegral $ length es) (fromIntegral $ length ns)
+-- -- Model fitting. 
+-- fitERG :: Graph -> Model
+-- fitERG gr@(Graph ns es) = ERG [ density ]
+--   where
+--     density = (/) (fromIntegral $ length es) (fromIntegral $ length ns)
