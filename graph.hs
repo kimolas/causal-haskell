9c6eb294cf1548798a8a66dad1f07c71a5b6b062
kimolas <nicolask@andrew.cmu.edu>
2015-04-22 23:53:59 -0400

    Refactored types; basically, "Graph a", where a is usually (Ord a, Eq a). Type a used to be [Int].

--- graph.hs
+++ graph.hs
@@ -4,6 +4,8 @@
 
 import qualified Data.Map as M
 import qualified Data.List as L
+import qualified Numeric.LinearAlgebra as LA
+import qualified System.Random as R
 
 -- Some examples of graphs. 
 gr1 = Graph [1..3] [(1,2), (2,1), (1,3)]
@@ -11,42 +13,43 @@ gr2 = Graph [1..2] [(1,2), (2,1), (1,3)]
 gr3 = completeGraph [1..10] 
 
 
-data Graph = Graph { nodes :: [Int]
-                   , edges :: [(Int, Int)]
-                   } deriving (Show, Eq)
+data Graph a = Graph { nodes :: [a]
+                     , edges :: [(a, a)]
+                     } deriving (Show, Eq)
 
 data Statistic = Statistic { values :: [Double] } deriving (Show, Eq)
 
 data Model = ERG { parameters :: [Double] } deriving (Show, Eq)
 
 -- Makes an empty graph. 
-emptyGraph :: Graph
+emptyGraph :: Graph a
 emptyGraph = Graph [] []
 
 -- Makes a complete simple graph from a list of nodes. 
-completeGraph :: [Int] -> Graph
-completeGraph ns = Graph ns (redundant es)
+completeGraph :: (Ord a, Eq a) => [a] -> Graph a
+completeGraph ns = Graph sns (redundant es)
   where
-    es = [ (x, y) | x <- ns, y <- ns , x /= y]
+    sns = L.sort ns
+    es = [ (x, y) | x <- sns, y <- sns , x /= y]
 
 -- Makes a valid graph object from a list of edges. 
-toGraph :: [(Int, Int)] -> Graph
+toGraph :: (Ord a, Eq a) => [(a, a)] -> Graph a
 toGraph es = Graph (extractNodes es) es
 
 -- Checks if the edges contain defined nodes. 
-validGraph :: Graph -> Bool
+validGraph :: (Eq a) => Graph a -> Bool
 validGraph (Graph ns es) = subset (L.union (map fst es) (map snd es)) ns
   where
     subset xs ys = all (`elem` ys) xs
 
 -- Removes redundant edges (e.g. (1,2) = (2,1)). 
-redundant :: [(Int, Int)] -> [(Int, Int)]
+redundant :: (Ord a, Eq a) => [(a, a)] -> [(a, a)]
 redundant es = L.intersect es (map f es)
   where
     f (x, y) = (min x y, max x y)
 
 -- Makes a graph valid (see the `validGraph` function). 
-makeValid :: Graph -> Graph
+makeValid :: (Eq a) => Graph a -> Graph a
 makeValid gr@(Graph ns es)
   | validGraph gr = gr
   | otherwise = Graph (L.union ens ns) es
@@ -54,32 +57,41 @@ makeValid gr@(Graph ns es)
       ens = L.nub $ L.union (map fst es) (map snd es)
 
 -- From a list of edges, give the sorted and pruned list of nodes
-extractNodes :: [(Int, Int)] -> [Int]
+extractNodes :: (Ord a, Eq a) => [(a, a)] -> [a]
 extractNodes es = L.nub $ L.sort allnodes
   where
     allnodes = (map fst es) ++ (map snd es)
 
 -- Add a set of nodes to a graph using their labels. 
-addNodes :: Graph -> [Int] -> Graph
+addNodes :: (Ord a) => Graph a -> [a] -> Graph a
 addNodes (Graph ns es) nns = Graph (L.union ns nns) es
 
 -- Add a set of edges to a graph. 
-addEdges :: Graph -> [(Int, Int)] -> Graph
+addEdges :: (Ord a) => Graph a -> [(a, a)] -> Graph a
 addEdges (Graph ns es) ees = Graph ns (L.union es ees)
 
+-- -- Creates the adjacency matrix for a graph. 
+-- adjacency :: Graph -> Matrix Int
+-- adjacency (Graph ns es) = (n >< n) . (\x -> )
+--   where
+--     n = length ns
+--     x = [  | x <- 
 
--- Various graph statistics. 
+---- Various graph statistics. 
 -- Computes the number of nodes. 
-nNode :: Graph -> Int
+nNode :: Graph a -> Int
 nNode gr = length $ nodes gr
 
 -- Computes the number of edges. 
-nEdge :: Graph -> Int
+nEdge :: Graph a -> Int
 nEdge gr = length $ edges gr
+
+-- -- Counts the number of trianges. 
+-- nTriangle :: Graph -> Int
 
 
 -- Computes the graph density. 
-density :: Graph -> Statistic
+density :: Graph a -> Statistic
 density gr@(Graph ns es) = Statistic [ dense ]
   where
     dense = (/) (fromIntegral $ length es) (fromIntegral $ length ns)
