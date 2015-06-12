c6f66e0142a3a235007d0dfd1990d9381e32bbd5
kimolas <nicolask@andrew.cmu.edu>
2015-06-11 20:27:54 -0400

    Preliminary work on adjacency matrix structured-graphs.

--- Adjacency.hs
+++ Adjacency.hs
@@ -16,29 +16,44 @@ data Graph' a = Graph' { nodes' :: [a]
                        , edges' :: M.Map a [(a, a)]
                        } deriving (Show, Eq)
 
--- data Graph'' a = Graph'' { nodes'' :: [a]
---                          , edges'' :: Matrix 
---                          } deriving (Show, Eq)
+data Graph'' = Graph'' { nodes'' :: [Int]
+                       , edges'' :: Matrix Double
+                       } deriving (Show, Eq)
 
--- Converts the edge list to a dictionary structure. Takes up more space,
--- but it's faster for looking up what else is connected to a particular
--- node. 
-toDict :: (Ord a, Eq a) => Graph a -> Graph' a
-toDict gr@(Graph ns es) = Graph' ns d
+-- Converts the edge list to a map structure. Takes up more space, but it's
+-- faster for looking up what else is connected to a particular node. 
+toMap :: (Ord a, Eq a) => Graph a -> Graph' a
+toMap (Graph ns es) = Graph' ns es'
   where
     fInsert o m x = M.insertWith (++) (o x) [x] m
-    d = L.foldl' (fInsert snd) (L.foldl' (fInsert fst) M.empty es) es
+    es' = L.foldl' (fInsert snd) (L.foldl' (fInsert fst) M.empty es) es
 
--- -- Converts the edge list to an adjacency matrix structure. 
--- toAdjacency :: Graph a -> Graph' a
--- toAdjacency gr@(Graph ns es) = Graph' ns m
+-- Creates a (fst/snd)-biased dictionary for edges. 
+toMap' :: (Ord a, Eq a) => ((a, a) -> a) -> Graph a -> Graph' a
+toMap' f (Graph ns es) = Graph' ns es'
+  where
+    fInsert o m x = M.insertWith (++) (o x) [x] m
+    es' = L.foldl' (fInsert f) M.empty es
+
+-- Converts an edge map to an adjacency matrix structure. 
+-- toAdjacency :: Graph' a -> Graph''
+-- toAdjacency (Graph' ns es) = Graph'' ns m
 --   where
---     m = 
--- 
--- SVD approximation to a graph (uses k-element SVD of the adjacency
--- matrix)
--- svdGraph :: Int -> Graph' a -> Graph' a
+--     n = length ns
+--     m = (n >< n) . 
 
+-- Given a list of edges which are all connected to a node, generates
+-- a list of 0's and 1's denoting membership. 
+toList :: (Ord a, Eq a) => [a] -> [(a,a)] -> [a]
+toList (n:ns) es
+  | fst e  = 
+  | snd e  = 
+  | True   = 
+
+-- SVD approximation to a graph (uses k-element SVD of the adjacency
+-- matrix). 
+svdGraph :: Int -> Graph'' -> Graph''
+svdGraph k (Graph'' ns es) = Graph'' ns $ compress k es
 
 -- This function was written by Nicolas Favre-Felix. It returns an
 -- approximation of the given matrix, by taking only the k largest entries
