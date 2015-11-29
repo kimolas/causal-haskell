82e7086cabc4e1a91f2c0a5b10b0c0a8b6deb76f
Nicolas Kim <nicolask@andrew.cmu.edu>
2015-11-29 14:18:34 -0500

    wrote function for finding longest list

--- app/Local.hs
+++ app/Local.hs
@@ -23,19 +23,16 @@ gr1 = toMap $ Graph [1..7] [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4), (4, 
 
 -- Using folds to create the neighborhood layer. Depends on lazy evaluation
 -- to avoid actually constructing the entire array. 
--- nbhd :: Ord a => Graph' a -> a -> M.Map a Int
--- nbhd gr n = do xs <- map (freq gr) <$> local gr n
---                M.unionsWith (+) <$> sequence xs
-
--- Using folds to create the neighborhood layer. Depends on lazy evaluation
--- to avoid actually constructing the entire array. 
 nbhd :: Ord a => Graph' a -> a -> M.Map a Int
 nbhd gr n = flip M.union locmap . flip M.intersection locmap . M.unionsWith (+) $ map (freq gr) loc
   where
     loc = local gr n
     locmap = M.fromList . zip loc $ repeat 0
-                -- Want to use adjust/update to only add to the local nodes
+
+-- Find the longest list in a list of lists
+longest :: [[a]] -> [a]
+longest = L.maximumBy (\x y -> compare (length x) (length y))
 
 -- Infer communities
-community :: Ord a => Graph' a -> a -> [[[Double]]]
+-- community :: Ord a => Graph' a -> a -> [[[Double]]]
 community gr n = kmeans 2 . map (\x -> [fromIntegral x]) . M.elems $ nbhd gr n
