9bac7cdf06476effcd7badb2db53f4d4a5525df1
Nicolas Kim <nicolask@andrew.cmu.edu>
2015-11-29 01:09:46 -0500

    wrote first Local function

--- app/Local.hs
+++ app/Local.hs
@@ -4,6 +4,19 @@ module Local where
 
 import Adjacency
 import Data.KMeans
+import qualified Data.List as L
+import qualified Data.Map.Lazy as M
+import Control.Monad
 
--- Extract nodes; this initializes the associative array. 
+-- Frequencies of local nodes
+freq :: Ord a => Graph' a -> a -> Maybe (M.Map a Int)
+freq gr n = case loc of
+              Just l  -> Just $ L.foldl ins M.empty l
+              Nothing -> Nothing
+  where
+    loc = M.lookup n $ edges' gr
+    ins m k = M.insertWith (+) k (1 :: Int) m
+
+-- Using folds to create the neighborhood layers. Depends on lazy
+-- evaluation to avoid actually constructing the entire array. 
 
