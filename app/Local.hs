0cc8d93e7d5292e882cbf9eb45b5b709771b538f
Nicolas Kim <nicolask@andrew.cmu.edu>
2015-11-29 02:10:10 -0500

    updated new-template and flipped freq again

--- app/Local.hs
+++ app/Local.hs
@@ -9,12 +9,12 @@ import qualified Data.Map.Lazy as M
 import Control.Monad
 
 -- Frequencies of local nodes
-freq :: Ord a => a -> Graph' a -> Maybe (M.Map a Int)
-freq n gr = L.foldl ins M.empty <$> loc
+freq :: Ord a => Graph' a -> a -> Maybe (M.Map a Int)
+freq gr n = L.foldl ins M.empty <$> loc
   where
     loc = M.lookup n $ edges' gr
     ins m k = M.insertWith (+) k (1 :: Int) m
 
 -- Using folds to create the neighborhood layers. Depends on lazy
 -- evaluation to avoid actually constructing the entire array. 
-
+-- nbhd :: Ord a => Graph' a -> a -> Maybe [M.Map a Int]
